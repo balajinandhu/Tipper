0853af851108629bece3dccc4e30594b6c0ee4bd
bnarayanaswami <bnarayanaswami@paypal.com>
2017-03-19 12:31:03 -0700

    Added optional features
    Currency based on locale, thousand separators and make keyboard appear
    on app loading

--- Tipper/ViewController.swift
+++ Tipper/ViewController.swift
@@ -7,6 +7,43 @@
 //
 
 import UIKit
+import Foundation
+
+func getSymbolForCurrencyCode(code: String) -> String? {
+    let locale = NSLocale(localeIdentifier: code)
+    return locale.displayName(forKey: NSLocale.Key.currencySymbol, value: code)
+}
+
+
+extension String {
+    
+    // formatting text for currency textField
+    func currencyInputFormatting() -> String {
+        
+        var number: NSNumber!
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currencyAccounting
+        formatter.currencySymbol = getSymbolForCurrencyCode(code: Locale.current.currencyCode!)
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 2
+        
+        var amountWithPrefix = self
+        
+        // remove from String: "$", ".", ","
+        let regex = try! NSRegularExpression(pattern: "[^0-9]", options: .caseInsensitive)
+        amountWithPrefix = regex.stringByReplacingMatches(in: amountWithPrefix, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.characters.count), withTemplate: "")
+        
+        let double = (amountWithPrefix as NSString).doubleValue
+        number = NSNumber(value: (double / 100))
+        
+        // if first number is 0 or all numbers were deleted
+        guard number != 0 as NSNumber else {
+            return ""
+        }
+        
+        return formatter.string(from: number)!
+    }
+}
 
 class ViewController: UIViewController {
 
@@ -17,12 +54,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        let result = UserDefaults.standard.value(forKey: "default_tip_index") ?? 0
-       
-        print(result)
+        billField.becomeFirstResponder()
+    
     }
+    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -30,7 +65,6 @@ class ViewController: UIViewController {
         let result = UserDefaults.standard.value(forKey: "default_tip_index") ?? 0
         tipControl.selectedSegmentIndex = result as! Int
         calculateTip(self)
-        print(result)
     }
 
 
@@ -44,12 +78,24 @@ class ViewController: UIViewController {
     }
     
     @IBAction func calculateTip(_ sender: AnyObject) {
-        let tipPercentages = [0.1, 0.15, 0.25]
-       let bill = Double(billField.text!) ?? 0
+        if let amountString = billField.text?.currencyInputFormatting() {
+            billField.text = amountString
+        }
+        let fmt = NumberFormatter()
+        fmt.numberStyle = NumberFormatter.Style.decimal
+        fmt.locale = Locale.current
+        
+       let tipPercentages = [0.1, 0.15, 0.25]
+    
+        let newString = billField.text?.replacingOccurrences(of: getSymbolForCurrencyCode(code: Locale.current.currencyCode!)!, with: "")
+.replacingOccurrences(of: ",", with: "")
+
+        let bill = Double(newString!) ?? 0.0
+        print(bill)
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = tip + bill
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%0.2f", total)
+        tipLabel.text = getSymbolForCurrencyCode(code: Locale.current.currencyCode!)! + " " + fmt.string(for: tip)!
+        totalLabel.text = getSymbolForCurrencyCode(code: Locale.current.currencyCode!)! + " " + fmt.string(for: total)!
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
